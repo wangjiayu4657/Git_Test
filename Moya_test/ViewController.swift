966a8534f2e9f74ea038dfdb4bc5f2925ffeb9e9
250455844@qq.com <wjy18646668556>
2020-08-25 14:49:18 +0800

    添加新项目

---
+++ Moya_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moya_test
+//
+//  Created by wangjiayu on 2020/8/25.
+//  Copyright © 2020 wangjiayu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
