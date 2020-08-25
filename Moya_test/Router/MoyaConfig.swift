4b21a59a91e8588e26a6b8a320d3a682cee9fb0d
250455844@qq.com <wjy18646668556>
2020-08-25 15:21:08 +0800

    添加路由文件夹

---
+++ Moya_test/Router/MoyaConfig.swift
@@ -0,0 +1,9 @@
+//
+//  MoyaConfig.swift
+//  Moya_test
+//
+//  Created by wangjiayu on 2020/8/25.
+//  Copyright © 2020 wangjiayu. All rights reserved.
+//
+
+import Foundation
---
+++ Moya_test/Router/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Moya_test
+//
+//  Created by wangjiayu on 2020/8/25.
+//  Copyright © 2020 wangjiayu. All rights reserved.
+//
+
+import Foundation
